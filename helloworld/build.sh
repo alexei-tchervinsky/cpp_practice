b201308179388f01341c837d629567f297103e43
Alexei Tchervinsky <a.chervinskiy@emzior.ru>
2019-02-19 14:38:09 +0300

    Расширение helloworld.cpp

--- helloworld/build.sh
+++ helloworld/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo compile with gcc
-g++ -g -o helloworld --std=c++14 -Wall -Wextra -Wold-style-cast helloworld.c
+g++ -g -o helloworld --std=c++14 -Wall -Wextra -Wold-style-cast helloworld.cpp
 echo compilation complete
 echo run application with './helloworld'
 
