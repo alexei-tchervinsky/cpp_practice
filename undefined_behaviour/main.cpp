27e82e74f477c73cd1b60009a21a615f27fe5bd5
Alexei Tchervinsky <alexey.tchervinsky@gmail.com>
2019-02-20 00:38:57 +0300

    Обновлены примеры неопределенного поведения

--- undefined_behaviour/main.cpp
+++ undefined_behaviour/main.cpp
@@ -10,6 +10,7 @@ int main(void)
 
 /*1.*/ i = v[i++]; //undefined
 /*2.*/ i = i++; //undefined
+/*3.*/ i = 7, i++, i++; //well defined
 /*4.*/ i = i + 1; //well defined
 /*5.*/ i = ++i; //preincrement undefined too
 /*6.*/ int j = i << -2; //undefined сдвиг на отрицательное число разрядов
