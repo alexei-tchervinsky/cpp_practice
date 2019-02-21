4453c160aca9d5e71f3dd71522c62efb610de0d8
Alexei Tchervinsky <alexey.tchervinsky@gmail.com>
2019-02-21 14:16:42 +0300

    Печать аргументов командной строки на занятии 2019-02-20

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -4,6 +4,21 @@
 
 int main(int argc, char *argv[])
 {
+    std::cout << argc << std::endl;
+    std::cout << argv[0] << std::endl;
+    
+    if (argc == 2) {
+        std::cout << argv[1] << std::endl;
+    } else {
+        std::cout << "Invalid number of arguments" << std::endl;
+        return 10;
+    }
+    /*
+     argv[0]  char*
+     argv[1]  char*
+     */
+    
+    
 	std::cout << "Hello, world! :-)" << std::endl;
 	return 0;
 }
